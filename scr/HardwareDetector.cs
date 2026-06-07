3d6915be987c326ab2fe8ffe9d55dab24a515392
modulerainwharf69 <nujest3263435@gmail.com>
2026-06-07 19:15:53 +0300

    Create HardwareDetector.cs

---
+++ scr/HardwareDetector.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace UltimateGamingOptimizer
+{
+    public static class HardwareDetector
+    {
+        public static void Scan()
+        {
+            Console.WriteLine("Scanning hardware...");
+            Console.WriteLine("CPU Detected");
+            Console.WriteLine("GPU Detected");
+            Console.WriteLine("RAM Detected");
+        }
+    }
+}
